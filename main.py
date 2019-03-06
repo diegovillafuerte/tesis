bc7c61cef19f38c7be1bb8ae09ad24065423ed18
DIego Villafuerte Soraiz <diegovillafuertesoraiz@gmail.com>
2019-03-06 09:55:24 -0600

    Remove development server

--- main.py
+++ main.py
@@ -275,6 +275,6 @@ def showMyApplicants(company_id):
 
 
 if __name__ == '__main__':
-        app.secret_key = 'Super secret key'
-        app.debug = True
-        app.run(host='0.0.0.0', port=5000)
+        #app.secret_key = 'Super secret key'
+        #app.debug = True
+        app.run#(host='0.0.0.0', port=5000)
